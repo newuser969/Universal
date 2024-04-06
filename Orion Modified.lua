7ac8d6f8c956d2f8326b23c44b648ffd29e6e2f7
NightMare Scripts <86247482+newuser969@users.noreply.github.com>
2024-04-06 14:17:09 -0600

    Update Orion Modified.lua

--- Orion Modified.lua
+++ Orion Modified.lua
@@ -1,9 +1,9 @@
-local UserInputService = game:GetService("UserInputService")
-local TweenService = game:GetService("TweenService")
-local RunService = game:GetService("RunService")
-local LocalPlayer = game:GetService("Players").LocalPlayer
+local UserInputService = cloneref(game:GetService("UserInputService"))
+local TweenService = cloneref(game:GetService("TweenService"))
+local RunService = cloneref(game:GetService("RunService"))
+local LocalPlayer = cloneref(game:GetService("Players").LocalPlayer)
 local Mouse = LocalPlayer:GetMouse()
-local HttpService = game:GetService("HttpService")
+local HttpService = cloneref(game:GetService("HttpService"))
 local IsOnMobile = table.find({Enum.Platform.IOS, Enum.Platform.Android}, UserInputService:GetPlatform())
 local UserId = 6018
 
